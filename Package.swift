0798bd876afc0207fef0e41861b1ca7712b4b013
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2020-09-03 17:00:50 +0900

    Set deployment target up to iOS 9 in libwebp

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.7.0"),
-        .package(url: "https://github.com/shimastripe/libwebp-Xcode.git", from: "1.1.0")
+        .package(url: "https://github.com/shimastripe/libwebp-Xcode.git", from: "1.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
